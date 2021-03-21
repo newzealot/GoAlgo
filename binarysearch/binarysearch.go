d91d0c468d7fee85cb11a59d2cebf89dcac1fd5c
newzealot <github@newzealot.com>
2021-03-21 18:13:51 +0800

    Refactored BinarySearch to closer match Colt Steele's "model answer"

--- binarysearch/binarysearch.go
+++ binarysearch/binarysearch.go
@@ -1,19 +1,26 @@
 package binarysearch
 
-func BinarySearch(arr []string, search string) (int, bool) {
-	lIndex := 0
-	rIndex := len(arr) - 1
-	for lIndex <= rIndex {
-		mIndex := (rIndex + lIndex) / 2
-		if arr[mIndex] == search {
-			return mIndex, true
+func BinarySearch(arr []string, s string) (int, bool) {
+	if len(arr) == 0 {
+		return 0, false
+	}
+	start, end := 0, len(arr)-1
+	// integer division will round down the result
+	mid := (start + end) / 2
+	// when start is bigger than end, it means cannot find s string
+	for arr[mid] != s && start <= end {
+		if s > arr[mid] {
+			// increment is to prevent stalling at mid, making the next start closer to the end
+			start = mid + 1
+		} else {
+			// decrement is to prevent stalling at mid, making the end closer to the start
+			end = mid - 1
 		}
-		if arr[mIndex] > search {
-			rIndex = mIndex - 1
-		}
-		if arr[mIndex] < search {
-			lIndex = mIndex + 1
-		}
+		mid = (start + end) / 2
+	}
+	if arr[mid] == s {
+		return mid, true
 	}
 	return 0, false
+
 }
